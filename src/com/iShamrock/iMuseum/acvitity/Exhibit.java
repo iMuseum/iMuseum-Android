752c18c0732647842f5a403acbb5f8fc5d4d5b06
wangxin <14302010064@fudan.edu.cn>
2015-11-25 18:53:53 +0800

    Exhibit layout

--- src/com/iShamrock/iMuseum/acvitity/Exhibit.java
+++ src/com/iShamrock/iMuseum/acvitity/Exhibit.java
@@ -1,8 +1,18 @@
 package com.iShamrock.iMuseum.acvitity;
 
+import android.app.Activity;
+import android.os.Bundle;
+import com.iShamrock.iMuseum.R;
+
 /**
  * Created by lifengshuang on 11/22/15.
  */
-public class Exhibit {
+public class Exhibit extends Activity{
     //wx start here
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exhibit);
+    }
 }
