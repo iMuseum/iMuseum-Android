b6727b39c337c7a427730a9b1b73a3c6bac79fa2
wangxin <14302010064@fudan.edu.cn>
2015-11-26 14:33:32 +0800

    Exibition back function

--- src/com/iShamrock/iMuseum/acvitity/Exhibit.java
+++ src/com/iShamrock/iMuseum/acvitity/Exhibit.java
@@ -1,9 +1,9 @@
 package com.iShamrock.iMuseum.acvitity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import com.iShamrock.iMuseum.R;
@@ -14,6 +14,7 @@ import com.iShamrock.iMuseum.R;
 public class Exhibit extends Activity{
     //wx start here
     private LinearLayout likeLayout;
+    private ImageButton back;
     private ImageButton like;
     private ImageButton go;
     /* the boolean isLiked is true when the user have liked it */
@@ -22,9 +23,19 @@ public class Exhibit extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exhibit);
+        Activity activity = this;
         likeLayout = (LinearLayout)this.findViewById(R.id.likeLayout);
+        back = (ImageButton)this.findViewById(R.id.Back);
         like = (ImageButton)this.findViewById(R.id.like);
         go = (ImageButton)this.findViewById(R.id.go);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(activity, Vision.class);
+                startActivity(intent);
+            }
+        });
         /* when the user click the like button, change the image it shows */
         likeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
