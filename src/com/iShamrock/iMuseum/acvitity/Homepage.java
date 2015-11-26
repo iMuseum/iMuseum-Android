6ca867280fba932f52a030091c9f51f4ae2a886d
lifengshuang <b2010000327@gmail.com>
2015-11-26 15:21:44 +0800

    debug homepage

--- src/com/iShamrock/iMuseum/acvitity/Homepage.java
+++ src/com/iShamrock/iMuseum/acvitity/Homepage.java
@@ -2,14 +2,14 @@ package com.iShamrock.iMuseum.acvitity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.animation.Animation;
+import android.widget.*;
 import com.iShamrock.iMuseum.R;
 import com.iShamrock.iMuseum.util.DrawerAdapter;
 import com.iShamrock.iMuseum.util.DrawerItemOnClickAction;
@@ -18,14 +18,23 @@ import com.iShamrock.iMuseum.util.DrawerItemOnClickAction;
  * Created by lifengshuang on 11/18/15.
  */
 public class Homepage extends Activity{
+    private AnimationDrawable animationDrawable;
+    private ImageView ivAnimView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
-
-
         initLeftDrawer();
         //myz start here
+//        Button showPosition = (Button) findViewById(R.id.showMap);
+
+//        ivAnimView = (ImageView) findViewById(R.id.exhibitView);
+//        ivAnimView.setBackgroundResource(R.drawable.pic1_homepage);
+//        Object backgroundObject = ivAnimView.getBackground();
+//        animationDrawable = (AnimationDrawable) backgroundObject;
+//        animationDrawable.stop();
+//        animationDrawable.start();
     }
 
     private void initLeftDrawer() {
