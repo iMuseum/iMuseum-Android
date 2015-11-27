fb265790b569f3f0e1fe818b7f5b724db4ff1366
wangxin <14302010064@fudan.edu.cn>
2015-11-27 16:46:13 +0800

    Exibition back function

--- src/com/iShamrock/iMuseum/acvitity/Exhibit.java
+++ src/com/iShamrock/iMuseum/acvitity/Exhibit.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import com.iShamrock.iMuseum.R;
+import com.iShamrock.iMuseum.data.DataItem;
+import com.iShamrock.iMuseum.data.MuseumData;
 
 /**
  * Created by lifengshuang on 11/22/15.
@@ -23,7 +27,22 @@ public class Exhibit extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exhibit);
+        /* get the id of this exhibit */
+        Bundle bundle = this.getIntent().getExtras();
+        int id = bundle.getInt("id");
+        /* get the information of this exhibit */
+        DataItem exhibit = MuseumData.getDataById(id);
+        TextView nameText = (TextView) this.findViewById(R.id.name);
+        ImageView exhibitView = (ImageView) this.findViewById(R.id.exhibitView);
+        TextView introduce = (TextView) this.findViewById(R.id.introduce);
+        /* set the title of the exhibit as the name */
+        nameText.setText(exhibit.getName());
+        /* set the image of the exhibit */
+        exhibitView.setImageDrawable(getResources().getDrawable(exhibit.getImgId()));
+        /* set the introduce of the exhibit */
+        introduce.setText("朝代：" + exhibit.getDynasty() + "  类型：" + exhibit.getType() + "\n" + exhibit.getDescription());
         Activity activity = this;
+        
         likeLayout = (LinearLayout)this.findViewById(R.id.likeLayout);
         back = (ImageButton)this.findViewById(R.id.Back);
         like = (ImageButton)this.findViewById(R.id.like);
