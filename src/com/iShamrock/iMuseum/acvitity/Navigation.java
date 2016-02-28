4ccb22fa5949d5e6948dfd7021cab395c237168c
wangxin <14302010064@fudan.edu.cn>
2016-02-28 21:16:32 +0800

    try LBS

--- src/com/iShamrock/iMuseum/acvitity/Navigation.java
+++ src/com/iShamrock/iMuseum/acvitity/Navigation.java
@@ -1,19 +1,17 @@
+
 package com.iShamrock.iMuseum.acvitity;
 
 import android.app.Activity;
 import android.os.Bundle;
 import com.iShamrock.iMuseum.R;
-import com.ids.model.map.City;
 import com.ids.sdk.android.map.*;
 import com.ids.sdk.android.map.Map;
-import com.ids.sdk.android.model.Building;
-import com.ids.sdk.android.model.RequestCallback;
+import com.ids.sdk.android.model.*;
 
 import java.util.List;
 
-
 /**
- * Created by wangxin on 16/2/28.
+ * Created by mayezhou on 16/2/26.
  */
 public class Navigation extends Activity {
     private MapView mapView;
@@ -23,23 +21,24 @@ public class Navigation extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation);
-        mapView = (MapView) findViewById(R.id.map_view);
+    /*    mapView = (MapView)findViewById(R.id.map_view);
         map = mapView.getMap(); //Map对象是地图显示的关键，对地图的调用都来自该对象。
-
+        //加载地图
         City.requestCity(new RequestCallback<List<City>>() {
             @Override
             public void onFinish(List<City> cities) {
                 //简单起见，使用第一个城市的第一个建筑
-                if (cities.get(0).getBuildingSet().size() > 0) if (cities.size() > 0) {
+                if (cities.size() > 0 && cities.get(0).getBuildingSet().size() > 0) {
                     Building building = cities.get(0).getBuildingSet().iterator().next();
                     map.init(building.getBuildingId()); //加载地图
                 }
             }
-
             @Override
             public void onFail() {
             }
         });
+        */
     }
+
 
 }
